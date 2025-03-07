279bed617bbb1613cfd9e14073fbabbb516f3db8
SongNathan <nadiasong332@gmail.com>
2025-03-07 12:32:44 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("\\    /\\"); // 백슬래시 2번
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
