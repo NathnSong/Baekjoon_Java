fb09331235b780d5a0b669e0ecb8a72d92bd6760
SongNathan <nadiasong332@gmail.com>
2025-05-17 23:24:39 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 28936 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 정수 N 입력
+		int N = Integer.parseInt(bf.readLine());
+		
+		// 큐 생성
+		Queue <Integer> que = new ArrayDeque<>();
+		for (int i = 1; i<N+1; i++) {
+			que.add(i);
+		}
+		
+		while(que.size()>1) {
+			que.poll();
+			que.add(que.poll());
+		}
+		
+		// 결과 출력
+		System.out.println(que.peek());
+	}
+
+}
